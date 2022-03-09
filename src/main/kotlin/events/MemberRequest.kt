3172b6de7c69e0f9cf70b09cd6d3ae1d0e154629
Colerar <233hbj@gmail.com>
2022-03-09 22:59:10 +0800

    Improve Log

--- src/main/kotlin/events/MemberRequest.kt
+++ src/main/kotlin/events/MemberRequest.kt
@@ -1,11 +1,13 @@
 package me.hbj.bikkuri.events
 
+import me.hbj.bikkuri.Bikkuri.logger
 import me.hbj.bikkuri.data.AutoApprove
 import me.hbj.bikkuri.data.AutoApproveData
 import me.hbj.bikkuri.data.ListenerData
 import net.mamoe.mirai.event.Event
 import net.mamoe.mirai.event.EventChannel
 import net.mamoe.mirai.event.events.MemberJoinRequestEvent
+import net.mamoe.mirai.utils.info
 
 fun EventChannel<Event>.onMemberRequest() {
   subscribeAlways<MemberJoinRequestEvent> {
@@ -13,11 +15,13 @@ fun EventChannel<Event>.onMemberRequest() {
     if (list.contains(fromId)) {
       it.accept()
       AutoApprove.map[groupId]?.set?.remove(fromId)
+      logger.info("Accepted MemberJoinRequestEvent because 'In auto approve list' for $this")
     }
   }
   subscribeAlways<MemberJoinRequestEvent> {
     if (!ListenerData.map.keys.contains(it.groupId)) return@subscribeAlways
     queuedMemberRequest.add(this)
+    logger.info { "Add MemberJoinRequestEvent to queue: $this" }
   }
 }
 
