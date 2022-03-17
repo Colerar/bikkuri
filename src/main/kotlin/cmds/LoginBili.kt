1ba443c904ecddea8bed397d5b02b9550cad2c1a
Colerar <233hbj@gmail.com>
2022-03-17 11:02:37 +0800

    add login way, notice when failed to send message

--- src/main/kotlin/cmds/LoginBili.kt
+++ src/main/kotlin/cmds/LoginBili.kt
@@ -4,24 +4,46 @@ import me.hbj.bikkuri.Bikkuri
 import me.hbj.bikkuri.client
 import moe.sdl.yabapi.api.getBasicInfo
 import moe.sdl.yabapi.api.logOut
+import moe.sdl.yabapi.api.loginWebConsole
 import moe.sdl.yabapi.api.loginWebQRCodeInteractive
+import moe.sdl.yabapi.api.loginWebSMSConsole
 import mu.KotlinLogging
+import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.ConsoleCommandSender
-import net.mamoe.mirai.console.command.SimpleCommand
 
 private val logger = KotlinLogging.logger {}
 
-object LoginBili : SimpleCommand(
-  Bikkuri, "loginbili",
+@Suppress("unused")
+object LoginBili : CompositeCommand(
+  Bikkuri, "loginbili", "bililogin",
   description = "登录 B 站帐号"
 ) {
-  @Handler
-  suspend fun @Suppress("unused") ConsoleCommandSender.login() {
-    if (client.getBasicInfo().data.isLogin) {
+  suspend fun logOutIfLogin() {
+    if (client.getBasicInfo().data.isLogin)
       client.logOut()
-    }
-    client.loginWebQRCodeInteractive {
-      logger.info(it)
-    }
+  }
+
+  @SubCommand("qr", "qrcode")
+  @Description("通过二维码登录B站帐号")
+  suspend fun ConsoleCommandSender.qr() {
+    logOutIfLogin()
+    client.loginWebQRCodeInteractive { Bikkuri.logger.info(it) }
+    Bikkuri.logger.info("Logged in successfully via qrcode")
+  }
+
+  @SubCommand("sms")
+  @Description("通过短信登录B站帐号")
+  suspend fun ConsoleCommandSender.sms() {
+    logOutIfLogin()
+    client.loginWebSMSConsole(true) { Bikkuri.logger.info(it) }
+    Bikkuri.logger.info("Logged in successfully via sms")
+  }
+
+  @SubCommand("pwd", "password")
+  @Description("通过密碼登录B站帐号")
+  suspend fun ConsoleCommandSender.pwd() {
+    logOutIfLogin()
+    client.loginWebConsole { Bikkuri.logger.info(it) }
+    Bikkuri.logger.info("Logged in successfully via pwd")
   }
 }
