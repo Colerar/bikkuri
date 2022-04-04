9745464b65d411cfffcc268d10383999f0643758
Colerar <233hbj@gmail.com>
2022-04-04 14:04:17 +0800

    Enhance random delay

--- src/main/kotlin/tasks/SetMessage.kt
+++ src/main/kotlin/tasks/SetMessage.kt
@@ -7,15 +7,22 @@ import me.hbj.bikkuri.Bikkuri
 import me.hbj.bikkuri.client
 import moe.sdl.yabapi.api.modifyMessageSetting
 import moe.sdl.yabapi.data.GeneralCode
-import net.mamoe.mirai.utils.info
 import net.mamoe.mirai.utils.warning
 
+private val logger = mu.KotlinLogging.logger { }
+
 fun CoroutineScope.setMessageTask(): Job = launch {
-  Bikkuri.logger.info { "Modifying message setting" }
-  client.modifyMessageSetting {
-    Intercept set off
-    FoldUnfollowed set off
-  }.also {
-    if (it.code != GeneralCode.SUCCESS) Bikkuri.logger.warning { "Failed to modify message setting: $it" }
+  logger.info { "Modifying message setting" }
+  runCatching {
+    client.modifyMessageSetting {
+      Intercept set off
+      FoldUnfollowed set off
+    }.also {
+      if (it.code != GeneralCode.SUCCESS) Bikkuri.logger.warning { "Failed to modify message setting: $it" }
+    }
+  }.onFailure {
+    if (it is IllegalArgumentException) {
+      logger.warn(it) { "Failed to modify message setting, may be not login" }
+    }
   }
 }
