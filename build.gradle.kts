2f25ae1ee681c7366d3fa33c14cace009f217921
Colerar <233hbj@gmail.com>
2023-04-10 13:48:03 +0800

    feat: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -158,6 +158,12 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
   kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
 }
 
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
+}
+
 tasks.withType<Test> {
   useJUnitPlatform()
 }
